30a81dfffd42a115d827cf04c270c6be1ccb0507
Max Tweddell <maxt.nz@gmail.com>
2017-07-21 10:14:30 +1200

    fix

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,29 @@
 var express = require('express')
 var router = express.Router()
+var db = require('../db')
 
-router.get('/', function (req, res) {
-  res.render('index', { hi: 'Hello World!' })
+router.get('/', function(req, res) {
+    res.render('index', { hi: 'Hello World!' })
 })
+
+router.get('/orders', function(req, res) {
+  db.getOrders(req.app.get('connection'))
+.then(function (orders) {
+    res.render('orders', { orders : orders })
+})
+}
+// router.get('/order-fulfil/:id', function(req, res) {
+//     res.render('index', { hi: 'Hello World!' })
+// })
+// router.get('/orders/new', function(req, res) {
+//     res.render('index', { hi: 'Hello World!' })
+// })
+// router.post('/orders/new', function(req, res) {
+//     res.render('index', { hi: 'Hello World!' })
+// })
+// router.get('/orders/:id', function(req, res) {
+//     res.render('index', { hi: 'Hello World!' })
+// })
+
 
 module.exports = router
