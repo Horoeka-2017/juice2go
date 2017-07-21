590f44c33ab6d92f88401e647eab2798db977e7b
Nathan Lamb <nathanlamb022@gmail.com>
2017-07-21 16:37:35 +1200

    fixed order link

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,15 @@ var express = require('express')
 var router = express.Router()
 var db = require('./db')
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.render('index', { hi: 'Hello World!' })
 })
 
-router.get('/orders/new', function(req, res) {
+router.get('/orders/new', function (req, res) {
     res.render('orders/new')
 })
 
-router.post('/orders/new', function(req, res, next) {
+router.post('/orders/new', function (req, res, next) {
     var orderFields = {
         name: req.body.name,
         address: req.body.address,
@@ -18,7 +18,7 @@ router.post('/orders/new', function(req, res, next) {
         delivery: req.body.delivery
     }
     var addOrder = db.addOrderInfo(orderFields, req.app.get('connection'))
-        .then(function(foo) {
+        .then(function (foo) {
             var itemFields = {
                 type: req.body.type,
                 quantity: req.body.quantity,
@@ -27,29 +27,29 @@ router.post('/orders/new', function(req, res, next) {
             }
 
             db.addItemInfo(itemFields, req.app.get('connection'))
-                .then(function() {
+                .then(function () {
                     var message = { message: 'Your order has been placed successfully! Huzzah!' }
                     res.redirect('/orders/new')
                 }, next)
         })
 })
 
-router.get('/orders/list', function(req, res) {
+router.get('/orders/list', function (req, res) {
     db.getOrders(req.app.get('connection'))
         .then((orders) => {
             res.render('orders', { orders: orders })
         })
 })
 
-router.get('/orders/:id', function(req, res) {
-    const id = req.params.id
+router.get('/orders/:id', function (req, res) {
+    const id = Number(req.params.id)
 
     db.getOrder(id, req.app.get('connection'))
-        .then(function(order) {
+        .then(function (order) {
             console.log(order)
             res.render('view', { order: order })
         })
-        .catch(function(err) {
+        .catch(function (err) {
             res.send(err.message)
         })
 })
