b9108dee006326b02f12dfbc2ed232495aee9721
max-tweddell <maxt.nz@gmail.com>
2017-07-21 12:47:13 +1200

    changed db

--- src/db.js
+++ src/db.js
@@ -1,39 +1,15 @@
 module.exports = {
-<<<<<<< HEAD
-  getUser: getUser,
-  getUsers: getUsers
-}
-
-=======
     getUser: getUser,
     getUsers: getUsers,
     getOrder: getOrder
 }
 
 
->>>>>>> master
 const config = require('./knexfile').development
 
 const knex = require('knex')
 const conn = knex(config)
 
-<<<<<<< HEAD
-function getUsers (connection) {
-  return connection('users').select()
-}
-
-function getUser (id, connection) {
-  return connection('users').where('id', id)
-}
-
-
-/**
-* This function gets all the order from the database, it should have no parameters
-*/
-
-function getOrders (){
-  return
-=======
 function getUsers(connection) {
     return conn('users').select()
 }
@@ -43,10 +19,6 @@ function getUser(id, connection) {
 }
 
 function getOrder(id, connection) {
-<<<<<<< HEAD
     return conn('orders').where('id', id)
-=======
-    return connection('orders').where('id', id)
->>>>>>> seeds
->>>>>>> master
+
 }
