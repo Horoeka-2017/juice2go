0f07ae4412f1d05cf44a92a992fa0b86f2b48c5c
joshua-fogg <joshuafoggnz@gmail.com>
2017-07-21 12:58:50 +1200

    fixed /orders syntax

--- src/routes.js
+++ src/routes.js
@@ -7,20 +7,20 @@ router.get('/', function(req, res) {
 })
 
 router.get('/orders', function(req, res) {
-  db.getOrders(req.app.get('connection'))
-    .then((orders) => {
-    res.render('orders', { orders : orders })
-})
-}
-// router.get('/order-fulfil/:id', function(req, res) {
-//     res.render('index', { hi: 'Hello World!' })
-// })
-// router.get('/orders/new', function(req, res) {
-//     res.render('index', { hi: 'Hello World!' })
-// })
-// router.post('/orders/new', function(req, res) {
-//     res.render('index', { hi: 'Hello World!' })
-// })
+        db.getOrders(req.app.get('connection'))
+            .then((orders) => {
+                res.render('orders', { orders: orders })
+            })
+    })
+    // router.get('/order-fulfil/:id', function(req, res) {
+    //     res.render('index', { hi: 'Hello World!' })
+    // })
+    // router.get('/orders/new', function(req, res) {
+    //     res.render('index', { hi: 'Hello World!' })
+    // })
+    // router.post('/orders/new', function(req, res) {
+    //     res.render('index', { hi: 'Hello World!' })
+    // })
 router.get('/orders/:id', function(req, res) {
     const id = req.params.id
     db.getOrder(id, (req.app.get('connection')))
